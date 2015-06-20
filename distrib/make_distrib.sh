81d17d4d0a95632f7e288a555b07ab40a0bc3fb0
Mario Juric <mjuric@lsst.org>
2015-06-20 01:32:47 -0700

    Support for self-extracting "packed" ipython distributable
    Added the ability to pack bin/ipython and python/eups_magic.py into a single
    file. The user should place this script onto their path (somewhere before the
    real IPython); once it's run, it will self-extract and run IPython with
    everything set up. Run distrib/make_distrib.sh to generate the packed script.
    
    This makes it very easy for the end user to "install" this product -- they
    just need to add the packed script to their path.

---
+++ distrib/make_distrib.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+BIN_IPYTHON="$DIR/../bin/ipython"
+EUPS_MAGIC="$DIR/../python/eups_magic.py"
+
+DISTRIB_IPYTHON="$DIR/ipython"
+
+# This must be the same as the eponymous variable in $BIN_IPYTHON script
+MAGIC="EMBEDDED""_PYTHON_MODULE----------"
+
+# Check we can orient ourselves
+if [[ ! -f "$BIN_IPYTHON" || ! -f "$EUPS_MAGIC" ]]; then
+	echo "$BIN_IPYTHON or $EUPS_MAGIC not found. Exiting."
+	exit -1
+fi
+
+# Copy ipython, inserting version
+head -n 1 "$BIN_IPYTHON" > "$DISTRIB_IPYTHON"
+echo "#" >> "$DISTRIB_IPYTHON"
+echo "# GENERATED FILE. DO NOT EDIT. THE SOURCE IS IN bin/ipython" >> "$DISTRIB_IPYTHON"
+echo "# THIS FILE WAS GENERATED WITH $(basename $0)." >> "$DISTRIB_IPYTHON"
+echo "#" >> "$DISTRIB_IPYTHON"
+echo "# git describe version: $(git describe --always --dirty)" >> "$DISTRIB_IPYTHON"
+echo "#" >> "$DISTRIB_IPYTHON"
+tail +2 "$BIN_IPYTHON" >> "$DISTRIB_IPYTHON"
+chmod +x "$DISTRIB_IPYTHON"
+
+echo "exit" >> "$DISTRIB_IPYTHON"
+echo >> "$DISTRIB_IPYTHON"
+echo "$MAGIC" >> "$DISTRIB_IPYTHON"
+cat "$EUPS_MAGIC" >> "$DISTRIB_IPYTHON"
+
+echo "The packed script has been created in $DISTRIB_IPYTHON"
